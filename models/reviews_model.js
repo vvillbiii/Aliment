b6c8551fa7b5d0522626e34725ae6658e3e7a206
vvillbii <will.bowles90@gmail.com>
2022-01-26 17:13:06 -0500

    updated restaurant route

--- models/reviews_model.js
+++ models/reviews_model.js
@@ -1,29 +1,32 @@
 const mongoose = require("mongoose");
 
-const reviewSchema = new mongoose.Schema(
-    {
-        rating: {
-            type: Number,
-            min: 1,
-            max: 5,
-            required: [true, "Please provide your rating here."]
-        },
-        body: {
-            type: String,
-            required: [true, "Fill out your feedback here."],
-        },
-        //relationship to User
-        user: {
-            type: mongoose.Schema.Types.ObjectId, 
-            ref: "User",},
-        
-        date: { 
-            type: Date, 
-            default: Date.now 
-        },
-        
-    }
-);
+const reviewSchema = new mongoose.Schema({
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    required: [true, "Please provide your rating here."],
+  },
+  body: {
+    type: String,
+    required: [true, "Fill out your feedback here."],
+  },
+  //relationship to User
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+
+  restaurant: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Restaurant",
+  },
+
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 const Review = mongoose.model("Review", reviewSchema);
 
